3cf0b0b781d209f46b634b9fe85fb16bc52590b4
rahulkhatar <160163114+rahulkhatar@users.noreply.github.com>
2025-12-23 14:49:23 +0530

    Update RestaurantControllerTests.cs

--- tests/Integration.Tests/RestaurantControllerTests.cs
+++ tests/Integration.Tests/RestaurantControllerTests.cs
@@ -15,5 +15,89 @@ namespace Integration.Tests;
 
 public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
+    private readonly WebApplicationFactory<Program> _factory;
+    private readonly Mock<IRestaurantService> _restaurantService = new();
+    public RestaurantControllerTests(WebApplicationFactory<Program> factory) 
+    {
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantService), _ => _restaurantService.Object));
+            });
+        });
+    }
     
+    [Fact]
+    public async Task GetAll_ForValidRequest_ReturnsOK()
+    {
+        //Arrange
+        var client = _factory.CreateClient();
+
+        //Act
+        var result = await client.GetAsync("api/Restaurants");
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetAll_ForInvalidRequest_ReturnsBadRequest()
+    {
+        //Arrange
+        var client = _factory.CreateClient();
+
+        //Act
+        var result = await client.GetAsync("api/restaurants/vdsv");
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+
+    //Policy
+    [Fact]
+    public async Task GetById_ForNonExistingId_ShouldReturn404NotFound()
+    {
+        //Arrange
+        int id = 123;
+        _restaurantService.Setup(m => m.GetRestaurantByIdAsync(id)).ReturnsAsync((Restaurant?)null);
+
+        var client = _factory.CreateClient();
+
+        //Act
+        var result = await client.GetAsync($"/api/restaurants/{id}");
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetById_ForExistingId_ShouldReturnOK()
+    {
+        //Arrange
+        int id = 123;
+        var restaurant = new Restaurant()
+        {
+            Id = id,
+            Name = "Test",
+            Description = "Test Description"
+        };
+
+        _restaurantService.Setup(m => m.GetRestaurantByIdAsync(id)).ReturnsAsync(restaurant);
+
+        var client = _factory.CreateClient();
+
+        //Act
+        var result = await client.GetAsync($"/api/restaurants/{id}");
+        var restaurantDto = await result.Content.ReadFromJsonAsync<RestaurantDto>();
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        restaurantDto.Should().NotBeNull();
+        restaurantDto.Name.Should().Be("Test");
+        restaurantDto.Description.Should().Be("Test Description");
+
+    }
 }
